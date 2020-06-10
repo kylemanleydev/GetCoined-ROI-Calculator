0ea4aa363826a5a42035b0c1375132ebe3894574
supermyle <kylemanley99@gmail.com>
2020-06-10 02:49:07 -0400

    Debugging

--- investcalc.js
+++ investcalc.js
@@ -30,34 +30,23 @@ window.onload = function() {
 	document.getElementById("date_field").setAttribute("max", yesterday);
 }
 
-//Calculate and print return on investment
-function calcROI() {
-	invest_amt = document.getElementById("invest_field").value;	
-	search_date = document.getElementById("date_field").value;
+//import our bitcoin current data from JSON
+let currentPriceURL = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json';
+let request = new XMLHttpRequest();
+request.open('GET', currentPriceURL);
+request.responseType = 'json';
+request.send();
 
-	//import our bitcoin current data from JSON
-	let currentPriceURL = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json';
-	let request = new XMLHttpRequest();
-	request.open('GET', currentPriceURL);
-	request.responseType = 'json';
-	request.send();
-	
-	console.log(request.response);
-
-	//import our bitcoin closing price data from JSON
-	let closingPriceURL = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2010-07-18&end=' + yesterday;
-	let request2 = new XMLHttpRequest();
-	request2.open('GET', closingPriceURL);
-	request2.responseType = 'json';
-	request2.send();
-
-	console.log("On " + search_date + " the price of BTC closed at $" + closingPrice
-	+ "\nthe price of BTC right now is $" + usdPrice + "/BTC\nYou spent $" + invest_amt + " on BTC");
-}
+//import our bitcoin closing price data from JSON
+let closingPriceURL = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2010-07-18&end=' + yesterday;
+let request2 = new XMLHttpRequest();
+request2.open('GET', closingPriceURL);
+request2.responseType = 'json';
+request2.send();
 
 request.onload = function() { //Run after requests are fetched from JSON URL
-    const btcpricedata = request.response; //get our json object for btc current price
-	console.log(btcpricedata);
+    const btcPriceData = request.response; //get our json object for btc current price
+	console.log(btcPriceData);
 
 	usdPrice = btcPriceData.bpi.USD.rate; //Grab Current USD price from JSON object
     console.log(usdPrice);	
@@ -66,4 +55,13 @@ request.onload = function() { //Run after requests are fetched from JSON URL
 request2.onload = function() { //Run after requests are fetched from JSON URL 
 	const closingPriceData = request2.response; //Get our JSON object for BTC Closing price
 	console.log(closingPriceData);
+}
+
+//Calculate and print return on investment
+function calcROI() {
+	invest_amt = document.getElementById("invest_field").value;	
+	search_date = document.getElementById("date_field").value;
+
+	console.log("On " + search_date + " the price of BTC closed at $" + closingPrice
+	+ "\nthe price of BTC right now is $" + usdPrice + "/BTC\nYou spent $" + invest_amt + " on BTC");
 }
